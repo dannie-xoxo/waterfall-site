afa87ee6158e16423752082d4f275760b745b218
Daniel Bryan <orisaekedanielbryan@gmail.com>
2022-09-27 20:17:27 +0100

    first commit

---
+++ src/components/Part1/part1.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../Part1/part1.css'
+import {BiPlanet} from 'react-icons/bi';
+import sky from '../images/img4.png';
+import mount1 from '../images/img5.png';
+import mount2 from '../images/img6.png';
+function Part1() {
+    return ( 
+        <>
+         <div className='container'>
+            <nav className='keyNav'>
+            <h1 className='header'> <span><BiPlanet /></span>Globe Express</h1>
+            <div className="border"></div>
+                <ul className= 'mainNav'>
+                <li className='navItem'><a className='active' href="#Home" >Home</a></li>
+                <li className='navItem'><a href="#menu" className='navLink'>Places</a></li>
+                <li className='navItem'><a href="#menu" className='navLink'>Destinations</a></li>
+                <li className='navItem'><a href="signIn" className='navLink'>Offers</a></li>
+                <li className='navItem'><a href="#create" className='navLink'>Contact Us</a></li>
+                </ul>
+            </nav>
+            <h1 className='big-header'>falls</h1>
+        <img className= 'sky' src={sky} alt='' />
+        <img className= 'mount1' src={mount1} alt='' />
+        <img className= 'mount2' src={mount2} alt='' />
+        </div>
+        </>
+     );
+}
+
+export default Part1;
