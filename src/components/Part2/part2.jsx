b07179741e8951fb86a3fb9484c455a581b085b8
Daniel Bryan <orisaekedanielbryan@gmail.com>
2022-10-03 12:56:49 +0100

    first commit

--- src/components/Part2/part2.jsx
+++ src/components/Part2/part2.jsx
@@ -7,8 +7,11 @@ import niagra from '../images/niagra.jpg';
 import { BiRightArrowAlt } from 'react-icons/bi';
 function Part2() {
 
+
   const[expand, setExpand] = useState('img');
-  const[ex, setEx] = useState('fig')
+  const[ex, setEx] = useState('fig');
+  const[text, setText]= useState('text')
+  const[cont, setCont]= useState('cont')
 
   const toggleHandler = () => {
     expand === 'img'
@@ -16,22 +19,66 @@ function Part2() {
    : setExpand('img');
 
    ex === 'fig'
-   ? setEx('fig fig-active')
+   ? setEx('fig fig-active') 
    : setEx('fig');
 
+   text === 'text'
+   ? setText('text text-active')
+   : setText('text');
+
+   cont === 'cont'
+   ? setCont('cont cont-active')
+   : setCont('cont');
+
   }
+ 
     return ( 
         <>
         <div className="container2">
             <h1 className="head">Waterfall Trip Ideas</h1>
             <div className="img-cont">
-            <figure className="cont">
-                <img  className={expand} src={angel} alt="" />
+            <figure className={cont} id='angel'>
+                <img className= {expand} src ={angel} alt="" />
                 <figcaption className={ex}>
                 <h3 className="feat">Feautured Destination</h3>
                 <h1 className="heading">Angel Falls</h1>
                 <div className="line"></div>
-                  <p className="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                  <p className={text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Magni iusto dolores maiores ipsa blanditiis quam deserunt voluptatum officia corporis, 
+                omnis distinctio facilis illo a explicabo eos, ad, rem sequi eveniet consequuntur
+                 necessitatibus similique quas sit vitae veniam. Molestias beatae aliquam nam eos nihil,
+                 illo laudantium rem temporibus quis reiciendis tempore!<br/>
+                 <button className="btn" onClick={toggleHandler}>Discover Destination <span><BiRightArrowAlt/></span></button></p>
+                </figcaption>
+                </figure>
+                 
+                 <figure className={cont}>
+                 <img className={expand} src={jog} alt='' />
+                <figcaption className= {ex}>
+                <h3 className="feat">Feautured Destination</h3>
+                <h1 className="heading">Jog Falls</h1>
+                <div className="line"></div>
+                <p className={text}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Aut temporibus, nisi assumenda quod libero incidunt?
+                  Doloribus asperiores praesentium inventore, pariatur
+                   illo ea magni tenetur suscipit rerum quod soluta.
+                    Maiores rerum id iste aliquid? Praesentium,
+                    impedit. Obcaecati ducimus numquam dolorem magnam ea iste error, 
+                neque vel, ut quaerat possimus voluptatem hic.<br/>
+                <button className="btn" onClick={toggleHandler}>Discover Destination <span><BiRightArrowAlt/></span></button></p>
+                </figcaption>
+                </figure>
+
+                </div>
+
+                <div className="img-cont1">
+            <figure className={cont}>
+            <img  className={expand} src={victoria} alt="" />
+                <figcaption className={ex}>
+                <h3 className="feat">Feautured Destination</h3>
+                <h1 className="heading">Victoria Falls</h1>
+                <div className="line"></div>
+                  <p className={text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
                 Magni iusto dolores maiores ipsa blanditiis quam deserunt voluptatum officia corporis, 
                 omnis distinctio facilis illo a explicabo eos, ad, rem sequi eveniet consequuntur
                  necessitatibus similique quas sit vitae veniam. Molestias beatae aliquam nam eos nihil,
@@ -40,55 +87,20 @@ function Part2() {
                 </figcaption>
                 </figure>
                 
-                 <figure className="cont">
-                 <img  className='img' src={jog} alt='' />
-                <figcaption className='fig'>
-                <h3 className="feat">Feautured Destination</h3>
-                <h1 className="heading">Jog Falls</h1>
-                <div className="line"></div>
-                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                 Aut temporibus, nisi assumenda quod libero incidunt?
-                  Doloribus asperiores praesentium inventore, pariatur
-                   illo ea magni tenetur suscipit rerum quod soluta.
-                    Maiores rerum id iste aliquid? Praesentium,
-                    impedit. Obcaecati ducimus numquam dolorem magnam ea iste error, 
-                neque vel, ut quaerat possimus voluptatem hic.<br/>
-                <button className="btn">Discover Destination <span><BiRightArrowAlt/></span></button></p>
-                </figcaption>
-                </figure>
-
-                </div>
-
-                <div className="img-cont1">
-            <figure className="cont">
-            <img  className='img' src={victoria} alt="" />
-                <figcaption className='fig'>
-                <h3 className="feat">Feautured Destination</h3>
-                <h1 className="heading">Victoria Falls</h1>
-                <div className="line"></div>
-                  <p className="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Magni iusto dolores maiores ipsa blanditiis quam deserunt voluptatum officia corporis, 
-                omnis distinctio facilis illo a explicabo eos, ad, rem sequi eveniet consequuntur
-                 necessitatibus similique quas sit vitae veniam. Molestias beatae aliquam nam eos nihil,
-                 illo laudantium rem temporibus quis reiciendis tempore!<br/>
-                 <button className="btn">Discover Destination <span><BiRightArrowAlt/></span></button></p>
-                </figcaption>
-                </figure>
-                
-                 <figure className="cont">
-                 <img  className='img' src={niagra} alt='' />
-                <figcaption className='fig'>
+                 <figure className={cont}>
+                 <img className={expand} id='niagra' src={niagra} alt='' />
+                <figcaption className={ex}>
                 <h3 className="feat">Feautured Destination</h3>
                 <h1 className="heading">Niagra Falls</h1>
                 <div className="line"></div>
-                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <p className={text}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                  Aut temporibus, nisi assumenda quod libero incidunt?
                   Doloribus asperiores praesentium inventore, pariatur
                    illo ea magni tenetur suscipit rerum quod soluta.
                     Maiores rerum id iste aliquid? Praesentium,
                     impedit. Obcaecati ducimus numquam dolorem magnam ea iste error, 
                 neque vel, ut quaerat possimus voluptatem hic.<br/>
-                <button className="btn">Discover Destination <span><BiRightArrowAlt/></span></button></p>
+                <button className="btn" onClick={toggleHandler}>Discover Destination <span><BiRightArrowAlt/></span></button></p>
                 </figcaption>
                 </figure>
 
